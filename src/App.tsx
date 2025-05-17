e4039318f03031879aa4c9ac4bd9c35f0c5451ac
ts2gamer22 <polarityclo@gmail.com>
2025-05-17 06:10:13 +0530

    last one hopefully

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from 'react';
+import { HashRouter, Routes, Route } from "react-router-dom";  // <-- change here
+import React from "react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -18,13 +17,13 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter> {/* <-- and here */}
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </QueryClientProvider>
     </React.StrictMode>
